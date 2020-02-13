ce0d8308056c1c85b2e716fad35a6e8f69e995cc
gokcelahmed <60474691+gokcelahmed@users.noreply.github.com>
2020-02-13 12:25:34 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,25 @@
-from flask import Flask
+import logging
+import os
 
+from flask import Flask
+import sqlalchemy
+
+db_user = os.environ.get("DB_USER")
+db_pass = os.environ.get("DB_PASS")
+db_name = os.environ.get("DB_NAME")
+cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
 
 app = Flask(__name__)
 
+logger = logging.getLogger()
 
-if app.config["ENV"] == "production":
-
-    app.config.from_object("config.ProductionConfig")
-
-elif app.config["ENV"] == "development":
-
-    app.config.from_object("config.DevelopmentConfig")
-
-else:
-
-    app.config.from_object("config.ProductionConfig")
-
+db = sqlalchemy.create_engine(
+    sqlalchemy.engine.url.URL(
+        drivername="mysql+pymysql",
+        username=db_user,
+        password=db_pass,
+        database=db_name,
+        query={"unix_socket": "/cloudsql/{}".format(cloud_sql_connection_name)},
+    ), pool_size=5, max_overflow=2, pool_timeout=30, pool_recycle=1800)
 
 from app import views
