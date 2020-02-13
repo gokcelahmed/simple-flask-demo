4e83a1ba017570ef3b2df43297fdf22a3c6a6d20
gokcelahmed <60474691+gokcelahmed@users.noreply.github.com>
2020-02-13 12:25:58 +0000

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,23 +1,74 @@
-from app import app
+from datetime import datetime
 
-from flask import request, render_template
+import sqlalchemy
+from flask import render_template, request, Response
+
+from app import app, db, logger
 
 
-@app.route("/")
+@app.before_first_request
+def create_tables():
+    # Create tables (if they don't already exist)
+    with db.connect() as conn:
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS votes "
+            "( vote_id SERIAL NOT NULL, time_cast timestamp NOT NULL, "
+            "candidate CHAR(6) NOT NULL, PRIMARY KEY (vote_id) );"
+        )
+
+
+@app.route("/", methods=["GET"])
 def index():
+    votes = []
+    with db.connect() as conn:
+        # Execute the query and fetch all results
+        recent_votes = conn.execute(
+            "SELECT candidate, time_cast FROM votes " "ORDER BY time_cast DESC LIMIT 5"
+        ).fetchall()
+        # Convert the results into a list of dicts representing votes
+        for row in recent_votes:
+            votes.append({"candidate": row[0], "time_cast": row[1]})
 
-    """
-    This route will render a template.
-    If a query string comes into the URL, it will return a parsed
-    dictionary of the query string keys & values, using request.args
-    """
+        stmt = sqlalchemy.text(
+            "SELECT COUNT(vote_id) FROM votes WHERE candidate=:candidate"
+        )
+        # Count number of votes for tabs
+        tab_result = conn.execute(stmt, candidate="TABS").fetchone()
+        tab_count = tab_result[0]
+        # Count number of votes for spaces
+        space_result = conn.execute(stmt, candidate="SPACES").fetchone()
+        space_count = space_result[0]
 
-    args = None
+    return render_template(
+        "index.html", recent_votes=votes, tab_count=tab_count, space_count=space_count
+    )
 
-    if request.args:
 
-        args = request.args
+@app.route("/", methods=["POST"])
+def save_vote():
+    # Get the team and time the vote was cast.
+    team = request.form["team"]
+    time_cast = datetime.datetime.utcnow()
+    # Verify that the team is one of the allowed options
+    if team != "TABS" and team != "SPACES":
+        logger.warning(team)
+        return Response(response="Invalid team specified.", status=400)
 
-        return render_template("public/index.html", args=args)
+    stmt = sqlalchemy.text(
+        "INSERT INTO votes (time_cast, candidate)" " VALUES (:time_cast, :candidate)"
+    )
+    try:
+        with db.connect() as conn:
+            conn.execute(stmt, time_cast=time_cast, candidate=team)
+    except Exception as e:
+        logger.exception(e)
+        return Response(
+            status=500,
+            response="Unable to successfully cast vote! Please check the "
+            "application logs for more details.",
+        )
 
-    return render_template("public/index.html", args=args)
+    return Response(
+        status=200,
+        response="Vote successfully cast for '{}' at time {}!".format(team, time_cast),
+    )
